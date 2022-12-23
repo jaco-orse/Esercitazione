3bc6211aa940b68fc07ef15673ac9dd98cf48e8c
jaco-orse <jaco.orse97@gmail.com>
2022-12-23 17:05:32 +0100

    SpringBootProject

---
+++ SpringDataRest/src/main/java/it/academy/corso/controller/JacoController.java
@@ -0,0 +1,30 @@
+package it.academy.corso.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class JacoController {
+
+    @GetMapping("/hiJaco")
+    public ResponseEntity<String> hiJaco(){
+        String message = "Il mio nome è Jaco";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/hiJaco2")
+    public ResponseEntity<Map<String,String>> hiJaco2 (){
+        Map <String,String> value = new HashMap();
+        value.put("Message","AUGURI");
+        return new ResponseEntity<>(value, HttpStatus.OK);
+    }
+
+
+}
