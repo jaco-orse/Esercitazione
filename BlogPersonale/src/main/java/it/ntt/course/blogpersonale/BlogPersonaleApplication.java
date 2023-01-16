0e0fcb0871f0da7f6055b9882b7a8997820aea97
jaco-orse <jaco.orse97@gmail.com>
2023-01-16 15:59:51 +0100

    Blog Personale versione 1

---
+++ BlogPersonale/src/main/java/it/ntt/course/blogpersonale/BlogPersonaleApplication.java
@@ -0,0 +1,13 @@
+package it.ntt.course.blogpersonale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPersonaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPersonaleApplication.class, args);
+	}
+
+}
---
+++ BlogPersonale/src/test/java/it/ntt/course/blogpersonale/BlogPersonaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ntt.course.blogpersonale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPersonaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
